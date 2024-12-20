99902ca4be0e875cef191c77523bf6c449a156f8
Rick Tuttle <rick@papasoft.com>
2024-12-20 15:30:40 -0500

    Upgrade to astro 5 and updated headshot

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_Z2wekXB from "../../assets/images/papasoft-lake.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/images/papasoft-lake.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z2wekXB]]);
+		
---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/papasoft/rick-tuttle.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpapasoft%2Frick-tuttle.mdx&astroContentModuleFlag=true")]]);
+		
